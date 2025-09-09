000c934434ddff1f093129ecc4652e080e37b279
julius-2027 <julius-neumann@gmx.net>
2025-09-09 12:19:08 -0500

    Update do_wham_test_cases.sh

--- do_wham_test_cases.sh
+++ do_wham_test_cases.sh
@@ -22,10 +22,10 @@ python do_wham.py windows_reus1_T310_N17_alpha.dat do_wham_py_test -xi_min 2 -xi
 
 python do_wham.py windows_reus1_T310_N17_alpha.dat do_wham_py_test -t_min 1000 -xi_max 3.5 -print_progress -plot
 
-# files not located within wham_v7 directory
+# files not located within this repository
 # python do_wham.py umbrella_test.win.txt do_wham_py_test_flatdata -print_progress -plot -kT 0.5961573
 # python do_wham.py umbrella_test.win.txt do_wham_py_test_flatdata -print_progress -plot -kT 0.8
 # python do_wham.py umbrella_test.win.txt do_wham_py_test_flatdata -print_progress -plot -kT 0.4
 
-# files not located within wham_v7 directory
+# files not located within this repository
 # python do_wham.py windows_fulldata_reus1_T310_N17_alpha.dat do_wham_py_test_fulldata -print_progress -plot
